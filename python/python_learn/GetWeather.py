1d20b73e95ea217aa047355fca34436f5b455f20
coolspan <1102494178@qq.com>
2017-09-25 16:59:04 +0800

    1

--- python/python_learn/GetWeather.py
+++ python/python_learn/GetWeather.py
@@ -12,7 +12,7 @@ def TestSuds() :
           imp = Import('http://www.w3.org/2001/XMLSchema', location='http://www.w3.org/2001/XMLSchema.xsd')
           imp.filter.add('http://WebXml.com.cn/')
           doc = ImportDoctor(imp)
-          url = 'http://www.webxml.com.cn/WebServices/WeatherWebService.asmx?wsdl'
+          url = 'http://ws.webxml.com.cn/WebServices/WeatherWebService.asmx?wsdl'
           client = Client(url, doctor=doc, cache=None)
           print (client)
           print (client.service.getWeatherbyCityName ('温州'))
