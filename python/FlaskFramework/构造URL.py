b3ccf8d0b1c63d2d008a6ac1e8cc100a660282ff
coolspan <1102494178@qq.com>
2018-01-25 15:06:11 +0800

    合并统一单位与家里python路径

---
+++ python/FlaskFramework/构造URL.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 –*-
+from flask import Flask, url_for
+app = Flask(__name__)
+@app.route('/')
+def index(): pass
+
+@app.route('/login')
+def login(): pass
+
+@app.route('/user/<username>')
+def profile(username): pass
+
+with app.test_request_context():
+    print(url_for('index'))# 调用index函数
+    print(url_for('login'))# 调用login函数
+    print(url_for('login', next='/',gz='gz'))
+    print(url_for('profile', username='John Doe'))
